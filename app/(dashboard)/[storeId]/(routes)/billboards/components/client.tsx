dc8ce25476f34ddbe383359ae0b9c5055ce029e6
Looky225 <1693570@bdeb.qc.ca>
2023-08-06 21:40:54 -0400

    Initial commit

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
+import { Plus } from "lucide-react";
+import { useRouter, useParams } from "next/navigation";
+
+
+interface BillboardClientProps {
+    data: Billboard[]
+}
+
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+    return(
+        <>
+            <div className="flex items-center justify-between">
+                <Heading 
+                    title={`Billboards (${data.length})`}
+                    description="Manage billboards for your store"
+                />
+
+                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                    <Plus className="mr-2 h-4 w-4"/>
+                    Add new
+                </Button>
+            </div>
+            <Separator />
+        </>
+    )
+}
